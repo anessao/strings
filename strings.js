de0637b384cda493ff4211b1c09d761da81a67e8
Anessa Ortner <anessa.ortner@gmail.com>
2017-02-18 12:44:18 -0600

    working functions

--- strings.js
+++ strings.js
@@ -1,16 +1,35 @@
-function reversal() {
+// var userInput = document.getElementById("letter-input");
+// var results = document.getElementById('results');
+// var submitButton = document.getElementbyId('submit');
 
+
+
+function reversal(x) {
+	var string = x.split('').reverse().join('');
+	console.log(string);
 }
 
-function alphabits() {
-
+function alphabits(x) {
+	var string = x.split('').sort().join('');
+	console.log(string);
 }
 
-function palindrome() {
-
+function palindrome(x) {
+	var string = x.split('').reverse().join('');
+	if (x === string) {
+		console.log("Your string is a palindrome!")
+	} else {
+		console.log("Your string is not a palindrome!");
+	}
 }
 
-var testString = "";
+var testString = "aba";
 reversal(testString);
 alphabits(testString);
 palindrome(testString);
+
+
+
+// submitButton.addEventListener("click", function myKeyPress(){
+
+// };
